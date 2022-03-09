4da24a1d1d1b2c5b5cea380c4621f4e14572f5e5
sanjalkatiyar <sanjaldhir@gmail.com>
2022-03-09 17:52:53 +0530

    Initial commit

---
+++ packages/components/dashboard/dashboard.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import PageHeading from '../../utils/heading/page-heading';
+import { HorizontalNav } from '@openshift-console/dynamic-plugin-sdk';
+import { Helmet } from 'react-helmet';
+import { useTranslation } from 'react-i18next';
+import { RouteComponentProps } from 'react-router';
+
+type SampleDashboardPageProps = {
+  history: RouteComponentProps['history'];
+};
+
+export const SampleDashboard: React.FC = () => {
+    return (
+    <>
+        <div className="co-dashboard-body">
+          <>{"Data Federation ~ Sample Dashboard"}</>
+        </div>
+    </>
+    );
+};
+
+export const SampleListPage: React.FC = () => {
+    return (
+        <>
+        <div className="co-dashboard-body">
+            <>{"Data Federation ~ Sample ListPage"}</>
+        </div>
+        </>
+    );
+};
+
+const SampleDashboardPage: React.FC<SampleDashboardPageProps> = (props) => {
+  const { t } = useTranslation('plugin__dfr-console');
+  const title = t('Data Federation Mock Page');
+  const pages = [
+    {
+      href: '',
+      name: t('Overview'),
+      component: SampleDashboard,
+    },
+    {
+      href: 'systems',
+      name: t('List Page'),
+      component: SampleListPage,
+    },
+  ];
+
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <PageHeading title={title} />
+      <HorizontalNav
+        pages={pages}
+        resource={{
+          kind: 'DataFederation',
+          apiVersion: 'console.odf.io/v1',
+        }}
+      />
+    </>
+  );
+};
+
+export default SampleDashboardPage;
---
+++ webpack.config.ts
@@ -0,0 +1,94 @@
+/* eslint-env node */
+
+import * as path from 'path';
+import { ConsoleRemotePlugin } from '@openshift-console/dynamic-plugin-sdk-webpack';
+import * as CopyWebpackPlugin from 'copy-webpack-plugin';
+import * as webpack from 'webpack';
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  entry: {},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]-bundle.js',
+    chunkFilename: '[name]-chunk.js',
+  },
+  watchOptions: {
+    ignored: ['node_modules', 'dist'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9001,
+    writeToDisk: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx?)|(\.tsx?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'cache-loader' },
+          { loader: 'thread-loader' },
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              outputStyle: 'compressed',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff2?|ttf|eot|otf)(\?.*$|$)/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[name].[ext]',
+        },
+      },
+    ],
+  },
+  plugins: [new ConsoleRemotePlugin(), new CopyWebpackPlugin({
+    patterns: [
+      { from: path.resolve(__dirname, 'locales'), to: 'locales' }
+    ]
+  })],
+  devtool: 'cheap-module-source-map',
+  optimization: {
+    chunkIds: 'named',
+    minimize: false,
+  }
+};
+
+export default config;
