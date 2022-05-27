ef09904f147d23fb167a01fdcdc7d59d11478625
Debjyoti Pandit <dpandit@redhat.com>
2022-05-27 17:10:53 +0530

    feat: status card for the admin dashboard

---
+++ packages/components/dashboard/status-card/status-card.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
+import { HealthBody } from '@openshift-console/dynamic-plugin-sdk-internal';
+import * as _ from 'lodash';
+import { useTranslation } from 'react-i18next';
+import {
+  Gallery,
+  GalleryItem,
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
+  Divider,
+} from '@patternfly/react-core';
+import { MCG_OPERATOR } from '../../../constants';
+import { ClusterServiceVersionKind } from '../../../types';
+import HealthItem from '../../../utils/dashboard/status-card/HealthItem';
+import { operatorResource } from '../../resources';
+import { getOperatorHealthState } from '../utils';
+import './status-card.scss';
+
+export const StatusCard: React.FC = () => {
+  const { t } = useTranslation();
+  const [csvData, csvLoaded, csvLoadError] =
+    useK8sWatchResource<ClusterServiceVersionKind[]>(operatorResource);
+
+  const operator = React.useMemo(
+    () => csvData?.find((csv) => csv.metadata.name.startsWith(MCG_OPERATOR)),
+    [csvData]
+  );
+  const operatorStatus = operator?.status?.phase;
+
+  const operatorHealthStatus = getOperatorHealthState(
+    operatorStatus,
+    !csvLoaded,
+    csvLoadError
+  );
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="status-card">{t('Status')}</CardTitle>
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <HealthBody>
+          <Gallery className="co-overview-status__health" hasGutter>
+            <GalleryItem>
+              <HealthItem
+                title={t('Data Federation service')}
+                state={operatorHealthStatus.state}
+              />
+            </GalleryItem>
+          </Gallery>
+        </HealthBody>
+      </CardBody>
+    </Card>
+  );
+};
---
+++ packages/components/dashboard/utils.ts
@@ -0,0 +1,23 @@
+import { HealthState } from '../../utils/dashboard/status-card/states';
+
+type SubsystemHealth = {
+  message?: string;
+  state: HealthState;
+};
+
+export const getOperatorHealthState = (
+  state: string,
+  loading,
+  loadError
+): SubsystemHealth => {
+  if (loading) {
+    return { state: HealthState.LOADING };
+  }
+  if (loadError) {
+    return { state: HealthState.NOT_AVAILABLE };
+  }
+  if (state === 'Succeeded') {
+    return { state: HealthState.OK };
+  }
+  return { state: HealthState.ERROR };
+};
