1741e79fe243b3d2b05dca65c2d5be6ade0f00dc
Debjyoti Pandit <dpandit@redhat.com>
2022-06-15 16:17:44 +0530

    feat: added popup for the buckets in data resource list view

--- packages/components/resource-pages/data-resource-list-view.tsx
+++ packages/components/resource-pages/data-resource-list-view.tsx
@@ -9,7 +9,11 @@ import {
 import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
 import { sortable } from '@patternfly/react-table';
-import { BucketClassType, NOOBAA_PROVIDER_MAP } from '../../constants';
+import {
+  BucketClassType,
+  BUCKET_CLASS_DETAILS_PATH,
+  NOOBAA_PROVIDER_MAP,
+} from '../../constants';
 import { NooBaaNamespaceStoreModel } from '../../models';
 import { BucketClassKind, NamespaceStoreKind } from '../../types';
 import { referenceForModel } from '../../utils';
@@ -20,6 +24,7 @@ import { GenericListPage } from '../../utils/list-page/list-page';
 import { LaunchModal } from '../../utils/modals/modalLauncher';
 import { getName } from '../../utils/selectors/k8s';
 import { bucketClassResource } from '../resources';
+import { MCGResourcePopOver } from './ResourcePopOver';
 
 const tableColumnInfo = [
   { className: '', id: 'name' },
@@ -43,7 +48,7 @@ export const RowRenderer: React.FC<RowProps<NamespaceStoreKind, CustomData>> =
     const { t } = useTranslation();
     const { launchModal, kebabActions, resourceMap } = rowData;
     const dataResourceName = getName(obj);
-    const bucketsCount = resourceMap[dataResourceName] || 0;
+    const bucketsCount = resourceMap[dataResourceName]?.length || 0;
     const path = `/mcgms/resource/${referenceForModel(
       NooBaaNamespaceStoreModel
     )}/${dataResourceName}`;
@@ -63,7 +68,16 @@ export const RowRenderer: React.FC<RowProps<NamespaceStoreKind, CustomData>> =
           {NOOBAA_PROVIDER_MAP[obj?.spec?.type]}
         </TableData>
         <TableData {...tableColumnInfo[3]} activeColumnIDs={activeColumnIDs}>
-          {t('{{bucketsCount}} Buckets', { bucketsCount })}
+          {bucketsCount ? (
+            <MCGResourcePopOver
+              label={t('{{bucketsCount}} Buckets', { bucketsCount })}
+              resourceList={resourceMap[dataResourceName]}
+              headerContent={t('Connected Buckets')}
+              resourceDetailsURL={BUCKET_CLASS_DETAILS_PATH}
+            />
+          ) : (
+            t('{{bucketsCount}} Buckets', { bucketsCount })
+          )}
         </TableData>
         <TableData {...tableColumnInfo[4]} activeColumnIDs={activeColumnIDs}>
           <Kebab
@@ -131,17 +145,23 @@ export const useDataResourceList = () => {
   const bucketPolicyMap = React.useMemo(
     () =>
       bucketPoliciesLoaded && !bucketPoliciesError
-        ? bucketPolicies?.reduce((bcMap, bc) => {
+        ? bucketPolicies?.reduce((bcMap: BucketPolicyMap, bc) => {
+            const bucketName = bc?.metadata?.name;
             const namespacePolicy = bc?.spec?.namespacePolicy;
             if (namespacePolicy?.type === BucketClassType.SINGLE) {
               const ns = namespacePolicy?.single?.resource;
-              bcMap[ns] = bcMap[ns] + 1 || 1;
+              bcMap[ns] = bcMap[ns] ? [...bcMap[ns], bucketName] : [bucketName];
             } else if (namespacePolicy?.type === BucketClassType.MULTI) {
               const dataResourceSet = new Set([
                 namespacePolicy?.multi?.writeResource,
                 ...namespacePolicy?.multi?.readResources,
               ]);
-              dataResourceSet.forEach((ns) => (bcMap[ns] = bcMap[ns] + 1 || 1));
+              dataResourceSet.forEach(
+                (ns) =>
+                  (bcMap[ns] = bcMap[ns]
+                    ? [...bcMap[ns], bucketName]
+                    : [bucketName])
+              );
             }
             return bcMap;
           }, {})
