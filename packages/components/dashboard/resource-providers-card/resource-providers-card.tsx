9dbffe98961cc0c74a8b54b6966e846248bc289e
Debjyoti Pandit <dpandit@redhat.com>
2022-07-27 20:17:11 +0530

    fix: added some css padding to various screens

--- packages/components/dashboard/resource-providers-card/resource-providers-card.tsx
+++ packages/components/dashboard/resource-providers-card/resource-providers-card.tsx
@@ -19,12 +19,10 @@ import './resource-providers-card.scss';
 const ResourceProvidersItem: React.FC<ResourceProvidersRowProps> =
   // eslint-disable-next-line react/display-name
   React.memo(({ title, count }) => (
-    <div className="co-inventory-card__item">
-      <div
-        className="nb-resource-providers-card__row-title"
-        data-test="nb-resource-providers-card"
-      >{`${count} ${title}`}</div>
-    </div>
+    <div
+      className="nb-resource-providers-card__row-title"
+      data-test="nb-resource-providers-card"
+    >{`${count} ${title}`}</div>
   ));
 
 const ResourceProviders: React.FC<{}> = () => {
